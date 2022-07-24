6ae412a97a396297f12667e8d6a3fa74c730e685
QuocTruong123a <102053117+QuocTruong123a@users.noreply.github.com>
2022-07-25 01:32:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,19 @@ use App\Http\Controllers\ControllerContact;
 Route::prefix('/')->group(function(){
 Route::controller(ControllerHome::class)->group(function(){
     Route::get('/Home','index')->name('home');
+    Route::post('/Book_tickes','book')->name('book');
     Route::get('/Payment','show')->name('payment');
+    Route::post('/post_Payment','post_show')->name('post.payment');
     Route::get('/notification_payment','notification')->name('notification');
+    Route::get('/pdf','pdf')->name('pdf');
 });
 Route::controller(ControllerEvent::class)->group(function(){
     Route::get('/Event','index')->name('event');
-    Route::get('Detailevent','show')->name('detailevent');
+    Route::get('Detailevent/{slug}/{id}','show')->name('detailevent');
 });
 Route::controller(ControllerContact::class)->group(function(){
     Route::get('/Contact','index')->name('contact');
+    Route::post('/send','send')->name('send.contact');
 });
 });
 
